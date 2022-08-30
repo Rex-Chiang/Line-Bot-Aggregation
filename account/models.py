99a3f521bdd1f312ba078ecd83e22368d3af8bac
Rex-Chiang <Rex.Chiang@luxshare-ict.com>
2022-08-30 17:01:14 +0800

    [chore] Update account fields

--- account/models.py
+++ account/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class Account(AbstractUser):
     id = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=64, unique=True)
-    user_name = models.CharField(max_length=32)
-    user_profile_pic = models.CharField(max_length=256, db_column="profile_pic")
-    user_language = models.CharField(max_length=10, default=None)
+    user_name = models.CharField(max_length=32, null=True)
+    user_profile_pic = models.CharField(max_length=256, null=True, db_column="profile_pic")
+    user_language = models.CharField(max_length=10, null=True, default=None)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
